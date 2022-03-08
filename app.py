799e591f978fe55359aa82cb2acc52369d25c42d
Abigail Akosua Addobea <31397625+aaddobea@users.noreply.github.com>
2022-03-08 12:38:55 +0000

    Change my name

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ import get_data as data
 st.set_page_config(page_title="WebScrapping - PY101", page_icon="🛒", layout="wide")
 st.image("https://unpackai.github.io/unpackai_logo.svg")
 st.title("WebScrapping of Lego Prices around the world 🌏")
-st.write("*by <name>*")
+st.write("*by <Abby sweet>*")
 # st.write("---")
